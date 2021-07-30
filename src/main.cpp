8fc59156197ea45bc883f7f0bad5c85ef682b883
Zachary Cauchi <zach.cauchi@hotmail.com>
2021-07-30 20:01:12 +0200

    feat: add time-duration printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include "loaders/scene-loader.hpp"
@@ -11,16 +12,27 @@ int main() {
     Scene s;
 
     // loader.LoadSceneFile("furnace.json", s);
-    loader.LoadSceneFile("assignment_03.json", s);
+    loader.LoadSceneFile("assignment_02.json", s);
 
     s.prepare();
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Rendering..." << std::endl;
     s.render();
+    std::cout << "\nDone" << std::endl;
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double, std::milli> timeTaken = t2 - t1;
+
+    std::cout << "Rendering completed in " << timeTaken.count() / 1000.0 << " seconds." << std::endl;
 
     // BoxBlur blurer = BoxBlur((size_t) s.renderer->width, (size_t) s.renderer->height);
-    // SigmoidalToneMapper sigTone = SigmoidalToneMapper((size_t) s.renderer->width, (size_t) s.renderer->height);
+    SigmoidalToneMapper sigTone = SigmoidalToneMapper((size_t) s.renderer->width, (size_t) s.renderer->height);
     // LinearToneMapper linTone = LinearToneMapper((size_t) s.renderer->width, (size_t) s.renderer->height);
 
-    // sigTone.postProcess(s);
+    sigTone.postProcess(s);
     // linTone.postProcess(s);
     // blurer.postProcess(s);
 
